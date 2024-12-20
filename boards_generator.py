dcdd03b00871f2852e78fa0021f8cdfb9be9f607
Ekaterina_Dul <ekaterina.dul@syngenta.com>
2024-12-20 13:28:38 +0400

    BoardGenerator quality check

--- boards_generator.py
+++ boards_generator.py
@@ -4,19 +4,38 @@ def print_matrix(matrix, n):
 
 
 class BoardGenerator:
+    # General board parameters:
     side = None
     words_with_descriptions = None
+    words_list = None
     matrix = None
+
+    # Board quality parameters:
+    current_number_of_intersections = None
+
+    # Words location metadata:
+    words_coordinates = None
 
     def __init__(self, side, words_with_descriptions):
         self.side = side
+
         self.words_with_descriptions = words_with_descriptions
+        self.words_list = list(words_with_descriptions.keys())
         self.matrix = ['#' * self.side for _ in range(self.side)]
+
+        self.current_number_of_intersections = -1
 
     def generate_board(self):
         self.__solve_puzzle(self.matrix, 0)
 
-    def __check_horizontal(self, x, y, matrix, current_word):
+    def get_number_of_intersections(self, matrix):
+        empty_places = sum([row.count('#') for row in matrix])
+        empty_places_wo_intersections = self.side * self.side - sum([len(word) for word in self.words_list])
+        return empty_places - empty_places_wo_intersections
+
+
+    @staticmethod
+    def __check_horizontal(x, y, matrix, current_word, word_coordinates):
         n = len(current_word)
         for i in range(n):
             if matrix[x][y + i] == '#' or matrix[x][y + i] == current_word[i]:
@@ -26,7 +45,8 @@ class BoardGenerator:
                 return matrix
         return matrix
 
-    def __check_vertical(self, x, y, matrix, current_word):
+    @staticmethod
+    def __check_vertical(x, y, matrix, current_word, word_coordinates):
         n = len(current_word)
         for i in range(n):
             if matrix[x + i][y] == '#' or matrix[x + i][y] == current_word[i]:
@@ -37,38 +57,35 @@ class BoardGenerator:
         return matrix
 
     def __solve_puzzle(self, matrix, index):
-        ways = [0]
+        words_coordinates = dict({word: None for word in self.words_list})
 
-        if index < len(self.words_with_descriptions):
-            current_word = self.words_with_descriptions[index]
+        if index < len(self.words_list):
+            current_word = self.words_list[index]
             max_len = self.side - len(current_word)
 
             for i in range(self.side):
                 for j in range(max_len + 1):
-                    temp = self.__check_vertical(j, i, matrix.copy(), current_word)
+                    temp = self.__check_vertical(j, i, matrix.copy(), current_word, words_coordinates.copy())
                     if temp[0][0] != '@':
                         self.__solve_puzzle(temp, index + 1)
 
             for i in range(self.side):
                 for j in range(max_len + 1):
-                    temp = self.__check_horizontal(i, j, matrix.copy(), current_word)
+                    temp = self.__check_horizontal(i, j, matrix.copy(), current_word, words_coordinates.copy())
                     if temp[0][0] != '@':
                         self.__solve_puzzle(temp, index + 1)
         else:
-            ways[0] += 1
-            print(f"{ways[0]} way to solve the puzzle")
-            print_matrix(matrix, self.side)
-            print()
+            if self.get_number_of_intersections(matrix) > self.current_number_of_intersections:
+                self.matrix = matrix
+                self.current_number_of_intersections = self.get_number_of_intersections(matrix)
 
 
-
-def main():
+if __name__ == "__main__":
     n1 = 3
-    words = ["MAM", "PAP"]
+    words = {"MAM": "sfva", "PAP": "vav"}
 
     board_generator = BoardGenerator(n1, words)
     board_generator.generate_board()
 
-
-if __name__ == "__main__":
-    main()
+    print_matrix(board_generator.matrix, board_generator.side)
+    print(board_generator.current_number_of_intersections)
